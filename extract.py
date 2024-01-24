e8913e8e9831feed4b41106f53ac6ae315e1a03e
Areel Khan <areel15511@gmail.com>
2024-01-24 02:18:19 -0500

    added experimentation

--- extract.py
+++ extract.py
@@ -2,23 +2,30 @@ import boto3
 import os
 import json
 
-print(json.load(open("secrets.json")))
+aws_secrets = json.load(open("secrets.json"))
 
 # S3 bucket and path
 BUCKET_NAME = 'oedi-data-lake'
-PREFIX = 'pvdaq/inverters/'
+PREFIX = 'pvdaq/parquet/metrics/'
 
 # Local directory to save the downloaded files
-local_directory = '/path/to/local/directory/'
+local_directory = 'data/'
 
-
-"""
 # Create an S3 client
-s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=region_name)
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=aws_secrets["aws_access_key_id"],
+    aws_secret_access_key=aws_secrets["aws_secret_key_id"],
+    region_name=aws_secrets["region_name"]
+    )
+
 
 # List objects in the specified S3 path
-objects = s3.list_objects(Bucket=bucket_name, Prefix=prefix)['Contents']
+objects = s3.list_objects(Bucket=BUCKET_NAME, Prefix=PREFIX, Delimiter="/")
 
+print(type(objects))
+
+"""
 # Download each object
 for obj in objects:
     key = obj['Key']
