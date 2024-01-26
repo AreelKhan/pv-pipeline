362af931a6688e54d8dee29242995ad8668f5f73
Areel Khan <areel15511@gmail.com>
2024-01-26 18:27:36 -0500

    creating dummy spark jon

--- dags/modules/pv_transform_spark.py
+++
@@ -1,8 +0,0 @@
-from pv_pipeline import PVTransform
-
-
-transformer = PVTransform(
-    staging_area=str(dag_run_conf.get("staging_area")),
-    logger=logging.getLogger(__name__)
-)
-transformer.transform(int(dag_run_conf.get("ss_id")))
--- dags/spark_submit_dag.py
+++ dags/spark_submit_dag.py
@@ -1,33 +1,31 @@
-import sys
-sys.path.append('/opt/airflow/dags/modules/') # hacky solution to import my ETL code
-
-from datetime import datetime
 from airflow import DAG
+from airflow.operators.python import PythonOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
-
-default_args = {
-    "owner": "Areel",
-    "start_date": datetime(2023, 12, 12),
-    "retries": 0
-}
-
 dag = DAG(
-    "test-spark-submit",
-    default_args=default_args,
-    description="testing spark submit",
-    schedule_interval=None,  # does not run on schedule
-    is_paused_upon_creation=False
+    dag_id="test_spark_submit",
+    default_args={
+        "owner": "Areel Khan"
+    }
 )
 
-
-
-load_task = SparkSubmitOperator(
-    task_id='load_metadata',
-    python_callable=load_metadata,
-    provide_context=True,
-    dag=dag,
+start = PythonOperator(
+    task_id="start",
+    python_callable=lambda: print("Jobs Started"),
+    dag=dag
 )
 
+python_job = SparkSubmitOperator(
+    task_id="python_job",
+    conn_id="spark-conn",
+    application="jobs/python/wordcountjob.py",
+    dag=dag
+)
 
-extract_task >> transform_task >> load_task
+end = PythonOperator(
+    task_id="end",
+    python_callable=lambda: print("Job completed"),
+    dag=dag
+)
+
+start >> python_job >> end
