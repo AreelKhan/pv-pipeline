e483b6fe00defde826f612d02e1621b150af2b1e
Areel Khan <areel15511@gmail.com>
2024-01-26 16:42:58 -0500

    updated scratch and ex[eriment files

--- spark/experiment.py
+++ spark/experiment.py
@@ -1,16 +1,30 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, functions
 
-# Create a Spark session
 spark = SparkSession.builder.appName("PV-merge-test").getOrCreate()
 
-# Define the column names
+metrics_cols = ["system_id", "metric_id", "sensor_name", "raw_units"]
+metrics_data = spark.read.parquet('staging_area/system10/metrics_system10.parquet').select(metrics_cols)
+filtered_metrics = metrics_data.filter(functions.col("sensor_name").isin(["dc_power", "ac_power", "poa_irradiance"]))
+
 pv_cols = ["measured_on", "metric_id", "value"]
+pv_data = spark.read.parquet('staging_area/system10/pv_data/*').select(pv_cols)
 
-# Read parquet files into a DataFrame
-pv_data = spark.read.parquet('staging_area/system_1349/pv_data/pv_data_system1349_2013-05-01.parquet').select(pv_cols)
+join_condition = pv_data["metric_id"] == filtered_metrics["metric_id"]
+merged = pv_data.join(filtered_metrics, join_condition, "inner")
 
-# Show the DataFrame
-pv_data.show()
+renames = {
+        "system_id":"ss_id",
+        "measured_on":"timestamp",
+        "sensor_name":"sensor",
+        "raw_units":"units"
+    }
+for old_name, new_name in renames.items():
+    merged = merged.withColumnRenamed(old_name, new_name)
 
-# Stop the Spark session
+selected_cols = ["ss_id", "timestamp", "sensor", "units", "value"]
+merged = merged.select(selected_cols)
+merged = merged.replace("", None)
+
+merged.write.parquet("staging_area/system10/pv_data_merged.parquet")
+
 spark.stop()
