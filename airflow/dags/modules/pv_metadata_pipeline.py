e64460f020af1fa83b581cfe4b9e01c9bedb0525
Areel Khan <areel15511@gmail.com>
2024-01-25 23:35:54 -0500

    created metadata transform step in airflow

--- airflow/dags/modules/pv_metadata_pipeline.py
+++ airflow/dags/modules/pv_metadata_pipeline.py
@@ -1,21 +1,24 @@
-from os import path, makedirs
+from os import path, makedirs, remove
 from base_pipeline import BasePipeline, BUCKET_NAME, SITE_PREFIX, MOUNT_PREFIX
+import pandas as pd
+from logging import Logger
 
 
 class MetadataExtract(BasePipeline):
 
-    def extract_metadata(self) -> None:
+    def extract(self) -> None:
         """
             Extracts PV system metadata
         """
+        self.logger.info("Extracting metadata from source...")
         makedirs(self.staging_area, exist_ok=True)
-        site_object = self.s3.list_objects(Bucket=BUCKET_NAME, Prefix=SITE_PREFIX, Delimiter="/")
 
+        site_object = self.s3.list_objects(Bucket=BUCKET_NAME, Prefix=SITE_PREFIX, Delimiter="/")
         try:
             site_key = site_object["Contents"][0]["Key"]
-            self.s3_download(site_key, path.join(self.staging_area, f"metadata.parquet"))
+            self.s3_download(site_key, path.join(self.staging_area, f"site.parquet"))
         except Exception as error:
-            self.logger.error(f"Error while extracting metadata: \n{error}")
+            self.logger.error(f"Error while extracting site data: \n{error}")
 
         mount_object = self.s3.list_objects(Bucket=BUCKET_NAME, Prefix=MOUNT_PREFIX, Delimiter="/")
         try:
@@ -24,15 +27,29 @@ class MetadataExtract(BasePipeline):
         except Exception as error:
             self.logger.error(f"Error while extracting mount data: \n{error}")
 
+        return None
 
-    def extract(self) -> None:
-        """
-            Extracts PV data and associated metrics, metadata and mount data for a given ss_id and date
-        """
-        # create staging area if it does not exist
-        makedirs(self.staging_area, exist_ok=True)
 
-        # check if system metadata exists, if not extract
-        if not path.isfile(path.join(self.staging_area, "metadata.parquet")):
-            self.logger.info("Metadata is not available. Extracting from source...")
-            self.extract_metadata()
+class MetadataTransform():
+    def __init__(self, staging_area: str, logger: Logger):
+        self.staging_area = staging_area
+        self.logger = logger
+
+    def transform(self):
+        try:
+            self.logger.info("Transforming metadata...")
+            metadata_cols = ["system_id", "latitude", "longitude", "elevation", "av_pressure", "av_temp", "climate_type"]
+            metadata = pd.read_parquet(path.join(self.staging_area, "site.parquet"), columns=metadata_cols)
+
+            mount_cols = ["system_id", "azimuth", "tilt"]
+            mount_data = pd.read_parquet(path.join(self.staging_area, "mount.parquet"), columns=mount_cols)
+
+            merged = pd.merge(metadata, mount_data, "inner", "system_id").rename({"system_id":"ss_id", "azimuth":"mount_azimuth", "tilt":"mount_tilt"}, axis=1)
+            merged.to_parquet(path.join(self.staging_area, "metadata.parquet"))
+            
+            remove(path.join(self.staging_area, "site.parquet"))
+            remove(path.join(self.staging_area, "mount.parquet"))
+        except Exception as error:
+            self.logger.error(f"Error while transforming metadata: \n{error}")
+
+        return None
--- airflow/dags/pv_metadata_etl.py
+++ airflow/dags/pv_metadata_etl.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('/opt/airflow/dags/modules/') # hacky solution to import my ETL code
 
 import logging
-from pv_metadata_pipeline import MetadataExtract
+from pv_metadata_pipeline import MetadataExtract, MetadataTransform
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
@@ -25,25 +25,24 @@ dag = DAG(
 
 
 def extract_metadata(**kwargs):
-    log = logging.getLogger(__name__)
-    
     dag_run_conf = kwargs["dag_run"].conf
-    staging_area: str = str(dag_run_conf.get("staging_area"))
-    aws_access_key_id: str = str(dag_run_conf.get("aws_access_key_id"))
-    aws_secret_access_key: str = str(dag_run_conf.get("aws_secret_access_key"))
-    region_name: str = str(dag_run_conf.get("region_name"))
-
     extractor = MetadataExtract(
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key,
-        region_name=region_name,
-        staging_area=staging_area,
-        logger=log
+        aws_access_key_id=str(dag_run_conf.get("aws_access_key_id")),
+        aws_secret_access_key=str(dag_run_conf.get("aws_secret_access_key")),
+        region_name=str(dag_run_conf.get("region_name")),
+        staging_area=str(dag_run_conf.get("staging_area")),
+        logger=logging.getLogger(__name__)
     )
     extractor.extract()
     return None
 
 def transform_metadata(**kwargs):
+    dag_run_conf = kwargs["dag_run"].conf
+    transformer = MetadataTransform(
+        staging_area=str(dag_run_conf.get("staging_area")),
+        logger=logging.getLogger(__name__)
+    )
+    transformer.transform()
     return None
 
 
